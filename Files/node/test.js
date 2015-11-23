d53b89ddc8a0757647b228c283c4b4971f3adf99
Chad Kosie <kosie150@gmail.com>
2015-11-22 18:59:36 -0600

    possible fix for img

--- Files/node/test.js
+++ Files/node/test.js
@@ -16,30 +16,66 @@ jsdom.env( html, function ( errors, window ) {
   var $ = JQuery( window );
   JCanvas( $, window);
 
+  var value = 5;
 
-  var $c  = $( '<canvas>' );
+  var $statCanvas = $('<canvas height="116px" width="220px" />');
+
+    fs.readFile(__dirname + '/images/extras/extras-9.png', function(err, squid) {
+        $statCanvas.drawText({
+            fillStyle: '#9cf',
+            strokeStyle: '#25a',
+            strokeWidth: 2,
+            x: 150, y: 50,
+            fontSize: 48,
+            fontFamily: 'Verdana, sans-serif',
+            text: value
+        });
+
+        console.log('attempting to draw image');
+        var img = new canvas.Image();
+        img.src = squid;
+
+        $statCanvas.drawImage({
+            source: img,
+            x: 0,
+            y: 0,
+            width: 70,
+            height: 116,
+            fromCenter: false,
+            load: function () {
+                console.log($statCanvas.getCanvasImage('png'));
+                // console.log($statCanvas[0].toDataURL());
+            }
+        });
+
+        // setTimeout(function () {
+        //     console.log($statCanvas.getCanvasImage('png'));
+        // }, 2000);
+    });
+
+
 
   // hack required by width/height bug in jsdom/node-canvas integration
-  $c[0].width = 400;
-  $c[0].height = 300;
-  var document = window.document;
-  Image = canvas.Image;
-
-    var img = new Image();
-    img.src = '../../images/extras/extras-9.png';
-
-    var can = $c[0];
-    var ctx = can.getContext('2d');
-
-    fs.readFile(__dirname + '../../images/extras/extras-9.png', function(err, squid){
-    if (err) throw err;
-      img = new Image;
-      img.src = squid;
-      ctx.drawImage(img, 0, 0, img.width, img.height);
-    //   console.warn( 'Paste the following as a url in your browser.' );
-      console.log(can.toDataURL());
-
-  });
+  // $c[0].width = 400;
+  // $c[0].height = 300;
+  // var document = window.document;
+  // Image = canvas.Image;
+  //
+  //   var img = new Image();
+  //   img.src = '../../images/extras/extras-9.png';
+  //
+  //   var can = $c[0];
+  //   var ctx = can.getContext('2d');
+  //
+  //   fs.readFile(__dirname + '../../images/extras/extras-9.png', function(err, squid){
+  //   if (err) throw err;
+  //     img = new Image;
+  //     img.src = squid;
+  //     ctx.drawImage(img, 0, 0, img.width, img.height);
+  //   //   console.warn( 'Paste the following as a url in your browser.' );
+  //     console.log(can.toDataURL());
+  //
+  // });
 
 
 
