3a2afd1095e1c06a45e318d86ca5eb019f511891
Chad Kosie <kosie150@gmail.com>
2015-12-03 20:42:15 -0600

    fix part of canvas size

--- Files/node/test.js
+++ Files/node/test.js
@@ -23,10 +23,10 @@ jsdom.env( html, function ( errors, window ) {
   $statCanvas[0].width = 200;
   $statCanvas[0].height = 200;
 
-  canvas.width = 200;
-  canvas.height = 200;
+  // canvas.width = 200;
+  // canvas.height = 200;
 
-    squid = fs.readFileSync(__dirname + '../../images/extras/extras-9.png');//, function(err, squid) {
+    squid = fs.readFileSync(__dirname + '/images/extras/extras-9.png');//, function(err, squid) {
         $statCanvas.drawText({
             fillStyle: '#9cf',
             strokeStyle: '#25a',
@@ -50,7 +50,7 @@ jsdom.env( html, function ( errors, window ) {
             height: 116,
             fromCenter: false,
             load: function () {
-                $statCanvas.getCanvasImage('png');
+                console.log($statCanvas.getCanvasImage('png'));
                 // console.log($statCanvas[0].toDataURL());
             }
         })
